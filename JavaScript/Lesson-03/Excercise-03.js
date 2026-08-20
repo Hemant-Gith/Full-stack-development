692e5e6244f36db999a79c0441ed6b14aa6b40db
Hemant-Gith <hg7714@outlook.com>
2026-08-20 22:37:59 +0530

    Practicced js basics

---
+++ JavaScript/Lesson-03/Excercise-03.js
@@ -0,0 +1,26 @@
+// JavaScript Tutorial Full Course - Beginner to Pro
+// Lesson 3 Exercises
+
+// 3a
+let text = 'My name is: ';
+
+// 3b
+let name = 'Hemant';
+
+// 3c - Concatenation
+let result = text + name;
+console.log(result);
+
+// 3d - Math + concatenation
+let coffee = 5;
+let bagel = 3;
+let total = coffee + bagel;
+
+console.log('Total cost: $' + total);
+
+// 3e - Template string + interpolation
+let message = `Total cost: $${total}`;
+console.log(message);
+
+// 3f - Alert
+alert(message);
